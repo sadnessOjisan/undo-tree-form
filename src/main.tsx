fede4e1119379f836cbcc8aa53216d7d26e61e91
sadnessOjisan <sadness.ojisan@gmail.com>
2020-05-19 20:28:32 +0900

    環境構築 (#1)
    * upd
    
    * chore reset

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./reset.css";
+
+const App = () => {
+  return <div>hello world</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
